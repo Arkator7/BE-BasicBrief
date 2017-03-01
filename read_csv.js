4a52526ce5b48365574726732c3e29fff8f9e276
Arkator7 <Arkator7@gmail.com>
2017-03-02 01:00:39 +1100

    code clean up, removing unused code, simplifying expression and splitting big functions

--- read_csv.js
+++ read_csv.js
@@ -1,8 +1,9 @@
-//Using D3 to read csv file and extract data
+//Using D3 to read CSV file and extract data
 function getPCList (pc) {
 	resultLib = [];
+
+	//read CSV files
 	d3.csv(websersrc, function(postcode) {
-		//console.log("postcode", postcode)
 
 		data = postcode.map(function(d) {
 			//each d is one line of the csv file represented as a json object
@@ -30,43 +31,50 @@ function getPCList (pc) {
 		})
 		//console.log("data", data)
 
-		//Draw table
-		d3.json('materials/return.json', function (error,data) {
-			function tabulate(data, columns) {
-				d3.select('body').select('table').remove();
-			
-				var table = d3.select('body').append('table');
-				var thead = table.append('thead');
-				var tbody = table.append('tbody');
-
-				// append the header row
-				thead.append('tr')
-					.selectAll('th')
-					.data(columns).enter()
-				    .append('th')
-					.text(function (column) { return column; });
-
-				// create a row for each object in the data
-				var rows = tbody.selectAll('tr')
-								.data(data)
-								.enter()
-								.append('tr');
-
-				// create a cell in each row for each column
-				var cells = rows.selectAll('td')
-								.data(function (row) {
-						return columns.map(function (column) {
-						return {column: column, value: row[column]};
-					});
-				})
-					.enter()
-					.append('td')
-					.text(function (d) { return d.value; });
-				return table;
-		}
-
-			// render the table(s)
-			tabulate(resultLib, ['postcode','suburb','state','dc','dc_type','lat','lon']);
-		});
+		//
+		var columns = ['postcode','suburb','state','dc','dc_type','lat','lon'];
+		drawTable(resultLib, columns);
 	});
 };
+
+function drawTable(data, columns) {
+	//Draw table
+	d3.json('materials/return.json', function (error, data) {
+		function tabulate(data, columns) {
+			//refresh/remove table state before drawing
+			d3.select('body').select('table').remove();
+			
+			var table = d3.select('body').append('table');
+			var thead = table.append('thead');
+			var tbody = table.append('tbody');
+
+			// append the header row
+			thead.append('tr')
+				.selectAll('th')
+				.data(columns).enter()
+			    .append('th')
+				.text(function (column) { return column; });
+
+			// create a row for each object in the data
+			var rows = tbody.selectAll('tr')
+							.data(data)
+							.enter()
+							.append('tr');
+
+			// create a cell in each row for each column
+			var cells = rows.selectAll('td')
+							.data(function (row) {
+					return columns.map(function (column) {
+					return {column: column, value: row[column]};
+				});
+			})
+				.enter()
+				.append('td')
+				.text(function (d) { return d.value; });
+			return table;
+		}
+
+		// render the table(s)
+		tabulate(resultLib, columns);
+	});
+}
